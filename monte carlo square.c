44239c76cbdfe2fc67ef092dd4447bf59411a260
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-03-30 22:22:39 +0530

    Create monte carlo square.c

---
+++ monte carlo square.c
@@ -0,0 +1,20 @@
+include <stdio.h> 
+
+int main{ 
+
+    int i,j;
+    double k1, k2; 
+    k2=.09; 
+
+     double arr[25]; 
+    for(i=0;i<=5;i++)
+    {
+        k1=214;
+        k2*=k2;
+
+        for(j=0;j<=5;j++)
+              arr[i,j] = k1*(1+k2);
+     }
+
+
+} 

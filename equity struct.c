812c7b672e3ffaed8db815ad24735b691044432b
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-04-05 22:24:47 +0530

    Create equity struct.c

---
+++ equity struct.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+typedef struct{ 
+
+
+float price;
+string ticker;
+
+
+}equity;
+
+main(){
+
+equity apple;
+
+apple.price=247;
+apple.ticker="Apple Corporation"; 
+
+
+return 0;
+
+}

4e1330bb0a804abbcae6a83c97d904d6982efcd8
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-07-10 23:51:34 -0400

    Create single linked linst.c

---
+++ single linked linst.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+struct node { 
+	int x;
+	struc node *next; 
+}; 
+
+int main(){
+
+
+	struct node *root; 
+
+	root = (struct node *) malloc( sizeof(struct node)) ;
+
+	root -> next = 0;
+
+	root -> 5; 
+	
+
+
+
+
+	return 0;
+}

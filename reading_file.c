dd826738398c31487cbb57708a6016c62ad1d055
sambhavu <30390294+sambhavu@users.noreply.github.com>
2020-09-01 23:56:01 -0400

    Create reading_file.c

---
+++ reading_file.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main() 
+{ 
+
+
+    int num; 
+    FILE *fptr; 
+    
+    if ((fptr = fopen("C:\\program.txt", "r")) ==NULL) { 
+        printf("Error! Opening file"); 
+        
+        exit(1); 
+    } 
+    
+    fscanf(fptr, "%d", &num); 
+    printf("Value of n = %d", num); 
+    
+    fclose(fptr); 
+    
+    
+    return 0;
+    
+    
+ } 
